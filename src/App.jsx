268a62a5a4caf013d86bad390d6dabcaae012424
Cristina <crn90@hotmail.es>
2026-05-26 13:16:36 +0200

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+function App() {
+  return (
+    <>
+      <header>Header AXUDI</header>
+
+      <main>
+        <section>Hero</section>
+        <section>Qué hacemos</section>
+        <section>Asóciate</section>
+        <section>Quiénes somos</section>
+        <section>Colaboradores</section>
+        <section>Noticias y eventos</section>
+        <section>Contacto</section>
+      </main>
+
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App

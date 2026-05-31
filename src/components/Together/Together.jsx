87a87e59994ce9011c9232ef2207855e68c00c13
Cristina <crn90@hotmail.es>
2026-05-31 11:46:04 +0200

    scss in progress

--- src/components/Together/Together.jsx
+++ src/components/Together/Together.jsx
@@ -1,25 +1,48 @@
-import Section from '../Section/Section';
+import togetherBg from '../../assets/images/im-juntosbig.webp';
+import bracketTop from '../../assets/logos/logoAxudi-ladoizq.svg';
+import bracketBottom from '../../assets/logos/logoAxudi-ladodrch.svg';
 
-// import './Together.scss';
+import './Together.scss';
 
 function Together() {
   return (
-    <Section className="together">
+    <section
+      className="together"
+      aria-labelledby="together-title"
+    >
+      <div
+        className="together__background"
+        style={{ backgroundImage: `url(${togetherBg})` }}
+      >
+        <div className="together__overlay">
 
-      <div className="together__overlay">
+          <div
+            className="together__decoration"
+            aria-hidden="true"
+          >
+            <img
+              className="together__bracket together__bracket--top"
+              src={bracketTop}
+              alt=""
+            />
 
-        <div
-          className="together__decoration"
-          aria-hidden="true"
-        ></div>
+            <img
+              className="together__bracket together__bracket--bottom"
+              src={bracketBottom}
+              alt=""
+            />
+          </div>
 
-        <blockquote className="together__title">
-          Juntos cambiamos la forma de vivir con diabetes
-        </blockquote>
+          <blockquote
+            id="together-title"
+            className="together__title"
+          >
+            Juntos cambiamos la forma de vivir con diabetes
+          </blockquote>
 
+        </div>
       </div>
-
-    </Section>
+    </section>
   );
 }
 
